075138f9214315e1f392d26ac9d86f75fda72d3b
aseruneko <aseruneko@gmail.com>
2022-05-23 18:47:53 +0900

    0.0.2: add h mode

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # json2html
 This .js makes .json to HTML elements concisely and clearly. Also can customize output elements by adding attributes to .json.
+
+## Usage
+
+/dist/json2html.jsを読み込むことで、html文書内のid="json2html"以下にdata.jsonの中身が展開されます。
+
+## ForDev
+
+webpack + typescript によりこのプロジェクトは作成されています。
+```:sh
+$ npm i
+$ npm start build
+```
+にて、src内のjson2html.tsをjson2html.jsへと変換できます。
+
+ただし、local環境においては、GoogleChromeはfileプロトコルでアクセスされたhtmlにおいてJSONファイルを読み込まない(CORSによる)ことに注意し、VSCodeのLive Serverエクステンションなどの使用などで回避してください。
--- src/json2html.ts
+++ src/json2html.ts
@@ -1,5 +1,6 @@
 const FILE_PATH: string = "./data.json";
 const TARGET_DIV_ID: string = "json2html";
+const USE_H_TAG: boolean = true;
 
 async function main(): Promise<void> {
     readJson(FILE_PATH).then(jsonObj => {
@@ -27,14 +28,47 @@ function jsonObjToData(jsonObj: object): Data[] {
     }
 }
 
-function makeHtmlElementIterator(data: Data): HTMLElement {
-    let element: HTMLElement = document.createElement("div");
-    element.appendChild(document.createTextNode(data.value()));
-    if(!data.hasData) {
+function makeHtmlElementForH(data: Data): HTMLElement {
+    const regex = /^\d{1,}$/;
+    function iterator(data: Data, recursiveLevel: number): [string, number][] {
+        if(!data.hasData()) {
+            return [[data.value(), 0]];
+        } else if (regex.test(data.value())) {
+            return data.data.flatMap(datum => iterator(datum, recursiveLevel));
+        } else {
+            const thisData: [string, number] = [data.value(), recursiveLevel];
+            const iterData = data.data.flatMap(datum => iterator(datum, recursiveLevel + 1));
+            return [thisData].concat(iterData);
+        }
+    }
+    const iteratedData = iterator(data, 1);
+    let element = document.createElement("div");
+    iteratedData.forEach(([datum, level]) => {
+        const tag = (level === 0 ? "p" : "h" + Math.min(level, 6));
+        let elem = document.createElement(tag)
+        elem.appendChild(document.createTextNode(datum))
+        element.appendChild(elem);
+    })
+    return element;
+}
+
+function makeHtmlElementIteratorForDiv(data: Data): HTMLElement {
+    const regex = /^\d{1,}$/;
+    if(!data.hasData()) {
+        let element: HTMLElement = document.createElement("div");
+        element.appendChild(document.createTextNode(data.value()));
+        return element;
+    } else if (regex.test(data.value())) {
+        let element: HTMLElement = document.createElement("div");
+        data.data.forEach(datum =>
+            element.appendChild(makeHtmlElementIteratorForDiv(datum))
+        );
         return element;
     } else {
+        let element: HTMLElement = document.createElement("div");
+        element.appendChild(document.createTextNode(data.value()));
         data.data.forEach(datum =>
-            element.appendChild(makeHtmlElementIterator(datum))
+            element.appendChild(makeHtmlElementIteratorForDiv(datum))
         );
         return element;
     }
@@ -42,7 +76,13 @@ function makeHtmlElementIterator(data: Data): HTMLElement {
 
 function makeHtmlElements(jsonObj: object): HTMLElement[] {
     const data = jsonObjToData(jsonObj);
-    return data.map(datum => makeHtmlElementIterator(datum));
+    return data.map(datum => {
+        if(USE_H_TAG) {
+            return makeHtmlElementForH(datum);
+        } else {
+            return makeHtmlElementIteratorForDiv(datum);
+        };
+    })
 }
 
 function appendHtmlElements(elements: HTMLElement[], target: string): void {
